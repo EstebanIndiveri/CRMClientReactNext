734b570464b0ff4f6e5dde79498bd033da3edd09
Esteban Indiveri <esteban.indiveri@gmail.com>
2020-09-26 01:28:48 -0300

    addApolloGraphql

--- pages/nuevacuenta.js
+++ pages/nuevacuenta.js
@@ -1,7 +1,51 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import {useFormik} from 'formik';
+import * as Yup from 'yup';
+import {useMutation,gql} from '@apollo/client';
+
+const NUEVA_CUENTA=gql`
+
+    mutation nuevoUsuario($input:UsuarioInput){
+        nuevoUsuario(input:$input){
+            id
+            nombre
+            apellido
+            email
+        }
+    }
+`;
 
 const NuevaCuenta = () => {
+
+    //mutation new user
+    const [nuevoUsuario]=useMutation(NUEVA_CUENTA);
+    
+
+    const formik=useFormik({
+        initialValues:{
+            nombre:'',
+            apellido:'',
+            email:'',
+            password:''
+        },
+        validationSchema:Yup.object({
+            nombre: Yup.string().required('El nombre es obligatorio'),
+            apellido:Yup.string().required('El apellido es obligatorio'),
+            email:Yup.string().email('El email no es válido').required('El email es obligatorio'),
+            password:Yup.string().required('El password no puede ir vacio').min(6,'El password debe ser de al menos 6 caracteres')
+
+        }),
+        onSubmit:valores=>{
+            console.log('enviando');
+            console.log(valores);
+
+            try
+        }
+    });
+        
+    if(loading)return (<div style={{height:'100vh',zIndex:1}} className="loader h-screen w-screen"></div>)
+
     return ( 
         <Layout>
             <h1 className="text-center text-2xl text-white font-light">Crear Nueva Cuenta</h1>
@@ -9,6 +53,7 @@ const NuevaCuenta = () => {
                 <div className="w-full max-w-sm">
                     <form
                     className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
+                    onSubmit={formik.handleSubmit}
                     >
                         <div className="mb-4">
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="nombre">
@@ -19,8 +64,17 @@ const NuevaCuenta = () => {
                             id="nombre"
                             type="text"
                             placeholder="Nombre Usuario"
+                            value={formik.values.nombre}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
                             />
                         </div>
+                        {formik.touched.nombre && formik.errors.nombre?(
+                            <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                                <p className="font-bold">Error</p>
+                                <p>{formik.errors.nombre}</p>
+                            </div>
+                        ):null}
 
                         <div className="mb-4">
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="apellido">
@@ -31,8 +85,16 @@ const NuevaCuenta = () => {
                             id="apellido"
                             type="text"
                             placeholder="Apellido Usuario"
+                            value={formik.values.apellido}
+                            onChange={formik.handleChange}
                             />
                         </div>
+                        {formik.touched.apellido && formik.errors.apellido?(
+                            <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                                <p className="font-bold">Error</p>
+                                <p>{formik.errors.apellido}</p>
+                            </div>
+                        ):null}
 
                         <div className="mb-4">
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
@@ -43,8 +105,16 @@ const NuevaCuenta = () => {
                             id="email"
                             type="email"
                             placeholder="Email Usuario"
+                            value={formik.values.email}
+                            onChange={formik.handleChange}
                             />
                         </div>
+                        {formik.touched.email && formik.errors.email?(
+                            <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                                <p className="font-bold">Error</p>
+                                <p>{formik.errors.email}</p>
+                            </div>
+                        ):null}
 
                         <div className="mb-4">
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
@@ -55,8 +125,16 @@ const NuevaCuenta = () => {
                             id="password"
                             type="password"
                             placeholder="Password Usuario"
+                            value={formik.values.password}
+                            onChange={formik.handleChange}
                             />
                         </div>
+                        {formik.touched.password && formik.errors.password?(
+                            <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                                <p className="font-bold">Error</p>
+                                <p>{formik.errors.password}</p>
+                            </div>
+                        ):null}
 
                         <input
                         type="submit"
