a2fe2e8876357f672b92f80e3fed9ab5e6becda6
Esteban Indiveri <esteban.indiveri@gmail.com>
2020-10-02 19:09:20 -0300

    add clientTable

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,14 @@ import Layout from '../components/Layout';
 import {gql,useQuery} from '@apollo/client';
 
 const OBTENER_CLIENTES_USUARIO=gql`
-  query obtenerClientes{
-    obtenerClientes{
-      nombre
-      email
-      empresa
-    }
+  query obtenerClientesVendedor{
+  obtenerClientesVendedor{
+    nombre
+    apellido
+    email
+    empresa
   }
+}
 `;
 
 export default function Home() {
@@ -17,12 +18,38 @@ export default function Home() {
   //apollo
   const{data,loading,error}=useQuery(OBTENER_CLIENTES_USUARIO);
 
-  // console.log(data);
+  console.log(data);
     if(loading)return (<div style={{height:'100vh',zIndex:1,opacity:0.7}} className="loader h-screen w-screen animate__animated animate__fadeIn"></div>)
     // console.log(loading);
   return (
     <Layout>
       <h1 className="text-2xl text-gray-800 font-light animate__animated animate__fadeIn animate__delay-1s">Clientes</h1>
+      {data.obtenerClientesVendedor.length===0 ? (<><h1 className="text-3xl animate__animated animate__fadeIn animate__delay-1s">No tienes clientes todavía</h1><br/>
+      <p className=" text-2xl  text-center animate__animated animate__fadeIn animate__delay-2s">¡Vamos a realizar ventas!</p>
+      </>)
+      :(
+        <table className="table-auto shadow-md mt-10 w-full w-lg animate__animated animate__fadeIn animate__delay-2s">
+        <thead className="bg-gray-800">
+          <tr className="text-white">
+            <th className="w-1/5 py-2">nombre</th>
+            <th className="w-1/5 py-2">Empresa</th>
+            <th className="w-1/5 py-2">Email</th>
+          </tr>
+        </thead>
+        <tbody className="bg-white">
+          {data.obtenerClientesVendedor.map(cliente=>(
+            <tr key={cliente.id}>
+              <td className="border px-4 py-2"> {cliente.nombre} {cliente.apellido}</td>
+              <td className="border px-4 py-2"> {cliente.empresa} </td>
+              <td className="border px-4 py-2"> {cliente.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      )
+      }
+
+     
     </Layout>
   )
 }
