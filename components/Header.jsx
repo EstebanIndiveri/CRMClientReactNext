68803f0bbb00966724a2bf3d71bcad4ed4675aed
Esteban Indiveri <esteban.indiveri@gmail.com>
2020-10-03 02:40:10 -0300

    createClient

--- components/Header.jsx
+++ components/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
     const router=useRouter();
     //query apollo
     const{data,loading,error}=useQuery(OBTENER_USUARIO);
-    console.log(data);
+    // console.log(data);
  
     if(loading)return (<div style={{height:'100vh',zIndex:1,opacity:0.7}} className="loader h-screen w-screen animate__animated animate__fadeIn"></div>)
     if(!data) return router.push('/login')
